065575d055b23694f6eabf36eff222e9892e76e4
Colt Carson <colt_carson@live.com>
2019-05-08 15:00:59 -0400

    Added App Icons

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,14 @@ void main() => runApp(
       MaterialApp(
         home: Scaffold(
           backgroundColor: Colors.blueGrey,
-          body: Center(
-            child: Image(
-              image: NetworkImage(
-                  'https://images-na.ssl-images-amazon.com/images/I/71w3Tto3QrL._SL1500_.jpg'),
-            ),
-          ),
           appBar: AppBar(
             title: Text('I Am Rich'),
             backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: AssetImage('images/diamond.png'),
+            ),
           ),
         ),
       ),
